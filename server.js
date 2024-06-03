ec65ad37f3cdc27a552b3d05d92327c1246140eb
TirkarParth <161726354+TirkarParth@users.noreply.github.com>
2024-06-03 22:26:54 +0200

    This setup ensures that the appropriate HTML file is served based on the URL path.
    Explanation of Changes:
    Import fs Module:
    
    const fs = require('fs'); is added to import the file system module which allows reading files from the filesystem.
    Parse the Request URL:
    
    const parsedUrl = url.parse(req.url, true); is used to parse the incoming request URL.
    Determine the File to Serve:
    
    let filePath; initializes a variable to store the path of the file to serve.
    if (parsedUrl.pathname.includes('documentation')) { filePath = './documentation.html'; } else { filePath = './index.html'; } checks if the URL path contains the word "documentation" and sets the filePath accordingly.
    Read and Serve the File:
    
    fs.readFile(filePath, (err, data) => { ... }); reads the content of the file specified by filePath.
    If an error occurs (e.g., file not found), a 404 error is returned.
    If the file is successfully read, its content is returned with a 200 status code and Content-Type set to text/html.
    Run the Server:
    
    Save the changes to server.js and run the server using node server.js.
    Testing:
    Accessing index.html:
    Open a web browser and go to http://localhost:8080. The content of index.html should be displayed.
    
    Accessing documentation.html:
    Open a web browser and go to http://localhost:8080/documentation. The content of documentation.html should be displayed.

--- server.js
+++ server.js
@@ -6,14 +6,51 @@ const http = require('http');
 // Import the url module
 const url = require('url');
 
+// Import the fs module to read files
+const fs = require('fs');
+
 // Define the port to listen on
 const PORT = 8080;
 
-// Create an HTTP server
-const server = http.createServer((req, res) => {
-  res.statusCode = 200; // Set the response status code to 200 (OK)
-  res.setHeader('Content-Type', 'text/plain'); // Set the content type to plain text
-  res.end('Hello, World!\n'); // Send a response to the client
+/// Create an HTTP server
+http.createServer((request, response) => {
+    // Parse the request URL
+    let addr = request.url;
+    let q = new URL(addr, 'http://' + request.headers.host);
+    let filePath = '';
+  
+    // Log the request URL and timestamp to log.txt
+    fs.appendFile(
+      'log.txt',
+      'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n',
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Added to log.');
+        }
+      }
+    );
+  
+    // Determine the file to serve based on the URL
+    if (q.pathname.includes('documentation')) {
+      filePath = __dirname + '/documentation.html';
+    } else {
+      filePath = __dirname + '/index.html';
+    }
+  
+    // Read the file and send its content to the client
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        response.writeHead(404, { 'Content-Type': 'text/plain' });
+        response.write('404 Not Found');
+        response.end();
+      } else {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(data);
+        response.end();
+      }
+    });
 });
 
 // Start the server and listen on the defined port
